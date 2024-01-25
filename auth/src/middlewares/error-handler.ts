d13b8a9e68887c5a88939f8f4fe210e2e40c09c5
santiago hurtado <sahurtaro@gmail.com>
2024-01-25 12:52:25 -0500

    145. Moving logic into errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,21 +4,12 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === 'field') {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    res.status(500).send({
-      errors: [
-        {
-          message: err.reason,
-        },
-      ],
+    res.status(err.statusCode).send({
+      errors: err.serializeErrors(),
     });
   }
 
