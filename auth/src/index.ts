d9dd281ea70d4a22169bf249c47843317e8bb2b8
santiago hurtado <sahurtaro@gmail.com>
2024-01-25 15:41:51 -0500

    149. How to define new custom errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/singout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -15,6 +16,9 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.get('*', () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
