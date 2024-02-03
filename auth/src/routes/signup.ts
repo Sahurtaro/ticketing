0106d5eab84319c49b5750a7e9cfa9e8778d8125
santiago hurtado <sahurtaro@gmail.com>
2024-02-02 19:07:01 -0500

    180. Accessing env variables in a pod

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -11,7 +11,10 @@ router.post(
   '/api/users/signup',
   [
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -30,7 +33,10 @@ router.post(
     await user.save();
 
     //Generate json web token
-    const userJwt = jwt.sign({ id: user.id, email: user.email }, 'asdf');
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
 
     //Store it on the session object
 
