5c61bf1ae24c58dba19c96d3750419852b2ae500
santiago hurtado <sahurtaro@gmail.com>
2024-02-05 10:28:57 -0500

    154. Common request validation middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -11,17 +12,10 @@ router.post(
   '/api/users/signup',
   [
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
@@ -33,10 +27,7 @@ router.post(
     await user.save();
 
     //Generate json web token
-    const userJwt = jwt.sign(
-      { id: user.id, email: user.email },
-      process.env.JWT_KEY!
-    );
+    const userJwt = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!);
 
     //Store it on the session object
 
