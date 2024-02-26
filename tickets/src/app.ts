7b2891993caf834dd3600c94f44474eee5f7258d
santiago hurtado <sahurtaro@gmail.com>
2024-02-26 17:47:20 -0500

    270. Adding auth protection

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import { errorHandler, NotFoundError } from '@sahurtarotickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@sahurtarotickets/common';
 import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,9 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
