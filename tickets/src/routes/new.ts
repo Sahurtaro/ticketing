8e0f1e8ca150ebaaad7c74f55b0f887122023514
santiago hurtado <sahurtaro@gmail.com>
2024-02-28 10:25:45 -0500

    275. Validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,20 @@
-import { requireAuth } from '@sahurtarotickets/common';
+import { requireAuth, validateRequest } from '@sahurtarotickets/common';
+import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('price must be greater than 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
