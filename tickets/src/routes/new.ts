0d930d3f09f7946ab526ea46bd4cb3dd388ef0b2
santiago hurtado <sahurtaro@gmail.com>
2024-04-04 11:35:26 -0500

    337. Graceful shutdown

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -20,7 +21,7 @@ router.post(
 
     await ticket.save();
 
-    await new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
